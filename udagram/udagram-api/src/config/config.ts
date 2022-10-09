6193e26a3254efc36cfdcd73e9fd169564449d56
MrZahran <abdozahran7@gmail.com>
2022-10-09 23:12:09 +0200

    New Changes

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,6 +11,7 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
+  port: process.env.PORT,
   dbPort: Number(process.env.DB_PORT),
 
   aws_region: process.env.AWS_REGION,
